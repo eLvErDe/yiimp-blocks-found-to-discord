cb0fadd3261858860a699708e920dd4374c9b371
Adam Cecile <acecile@le-vert.net>
2018-04-13 00:19:22 +0200

    Discords replies with 204

--- yiimp-blocks-found-to-discord.py
+++ yiimp-blocks-found-to-discord.py
@@ -180,7 +180,7 @@ async def post_events_discord(url, queue, d_markets_stocks_exchange, d_markets_c
                 with aiohttp.ClientSession() as session:
                     resp = await session.post(url, data=json.dumps({'content': message}), headers={'content-type': 'application/json'})
                     try:
-                        assert resp.status == 200, 'aiohttp call to %s failed with code %s' % (url, resp.status)
+                        assert resp.status == 204, 'aiohttp call to %s failed with code %s' % (url, resp.status)
                         await resp.release()
                     except Exception as e:
                         resp.close()
